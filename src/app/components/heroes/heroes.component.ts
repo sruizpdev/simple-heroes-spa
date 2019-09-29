ebc29caf65df239202af425b2aedf04601be5c5e
Sergio Ruiz <sergiotto@outlook.com>
2019-09-29 19:24:46 +0200

    Añadiendo el componente heroe

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from 'src/app/services/heroes.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,12 +10,16 @@ import { HeroesService, Heroe } from 'src/app/services/heroes.service';
 export class HeroesComponent implements OnInit {
   heroes: Heroe[] = [];
 
-  constructor(private _heroesService: HeroesService) {
+  constructor(private _heroesService: HeroesService,
+    private _router: Router) {
 
   }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
+  }
+  verHeroe(idx: number) {
+    this._router.navigate(['/heroe', idx])
   }
 
 }
