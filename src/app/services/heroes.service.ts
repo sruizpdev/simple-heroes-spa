e745206daf8b65ee629f5268dc47d5504bc3ca7c
Sergio Ruiz <sergiotto@outlook.com>
2019-09-29 18:02:42 +0200

    Se agrega servicio para mostrar los heroes

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,17 +1,22 @@
-[
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HeroesService {
+
+  private heroes: Heroe[] = [
     {
       nombre: "Aquaman",
       bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
       img: "assets/img/aquaman.png",
       aparicion: "1941-11-01",
-      casa:"DC"
+      casa: "DC"
     },
     {
       nombre: "Batman",
       bio: "Los rasgos principales de Batman se resumen en «destreza física, habilidades deductivas y obsesión». La mayor parte de las características básicas de los cómics han variado por las diferentes interpretaciones que le han dado al personaje.",
       img: "assets/img/batman.png",
       aparicion: "1939-05-01",
-      casa:"DC"
+      casa: "DC"
     },
     {
       nombre: "Daredevil",
@@ -25,7 +30,7 @@
       bio: "Su principal poder es su capacidad de aumentar su fuerza hasta niveles prácticamente ilimitados a la vez que aumenta su furia. Dependiendo de qué personalidad de Hulk esté al mando en ese momento (el Hulk Banner es el más débil, pero lo compensa con su inteligencia).",
       img: "assets/img/hulk.png",
       aparicion: "1962-05-01",
-      casa:"Marvel"
+      casa: "Marvel"
     },
     {
       nombre: "Linterna Verde",
@@ -49,3 +54,20 @@
       casa: "Marvel"
     }
   ];
+
+  constructor() {
+    console.log('servicio listo para su uso');
+  }
+
+  getHeroes() {
+    return this.heroes;
+  }
+
+}
+export interface Heroe {
+  nombre: string,
+  bio: string,
+  img: string,
+  aparicion: string,
+  casa: string,
+}
